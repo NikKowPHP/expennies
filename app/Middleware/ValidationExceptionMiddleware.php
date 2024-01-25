24e218b90400f9d36a5426a69362caf0e1dfed0e
NikKowPHP <nik.kow@outlook.com>
2024-01-25 12:57:30 +0100

    feat(ValidationMiddleware): Reference the url for dynamic rederection

--- app/Middleware/ValidationExceptionMiddleware.php
+++ app/Middleware/ValidationExceptionMiddleware.php
@@ -9,21 +9,23 @@ use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\ResponseInterface; 
 
 class ValidationExceptionMiddleware implements MiddlewareInterface
 {
-	public function __construct(private ResponseFactoryInterface $responseFactory)
+	public function __construct(private readonly ResponseFactoryInterface $responseFactory)
 	{
 	}
-	public function process(ServerRequestInterface $request, RequestHandlerInterface $handler)
+	public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
 	{
 		try {
 			return $handler->handle($request);
 
 		} catch (ValidationException $e) {
 			$response = $this->responseFactory->createResponse();
+			$referer = $request->getServerParams()['HTTP_REFERER'];
 
-			return $response->withHeader('Location', '/register')->withStatus(302);
+			return $response->withHeader('Location', $referer)->withStatus(302);
 
 
 		}
