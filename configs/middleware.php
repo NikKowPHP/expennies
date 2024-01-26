9c144f26db52f038bc53dc3228fc52b8bd418fa3
NikKowPHP <nik.kow@outlook.com>
2024-01-26 12:17:40 +0100

    feat(middleware): Register authentication middleware

--- configs/middleware.php
+++ configs/middleware.php
@@ -3,6 +3,7 @@
 declare(strict_types = 1);
 
 use App\Config;
+use App\Middleware\AuthenticateMiddleware;
 use App\Middleware\OldFormMiddleware;
 use App\Middleware\StartSessionsMiddleware;
 use App\Middleware\ValidationErrosMiddleware;
@@ -15,15 +16,13 @@ return function (App $app) {
     $container = $app->getContainer();
     $config    = $container->get(Config::class);
 
-    // Twig
+    $app->add(AuthenticateMiddleware::class);
     $app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
     $app->add(ValidationExceptionMiddleware::class);
     $app->add(ValidationErrosMiddleware::class);
     $app->add(OldFormMiddleware::class);
     $app->add(StartSessionsMiddleware::class);
 
-
-    // Logger
     $app->addErrorMiddleware(
         (bool) $config->get('display_error_details'),
         (bool) $config->get('log_errors'),
