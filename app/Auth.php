bdef6b2e74cc8d1b24d8f148a9ff6cbf7b6cd6fb
NikKowPHP <nik.kow@outlook.com>
2024-01-26 12:31:48 +0100

    feat(Auth): Introduce attempt login and logout functionality

--- app/Auth.php
+++ app/Auth.php
@@ -2,33 +2,62 @@
 declare(strict_types=1);
 
 namespace App;
+
 use App\Entity\User;
 use Doctrine\ORM\EntityManager;
 use App\Contracts\AuthInterface;
 use App\Contracts\UserInterface;
 
-class Auth implements AuthInterface 
+class Auth implements AuthInterface
 {
 	private ?UserInterface $user = null;
 	public function __construct(private readonly EntityManager $entityManager)
 	{
 
 	}
- public function user(): ?UserInterface
- {
-	if($this->user !== null){
+	public function user(): ?UserInterface
+	{
+		if ($this->user !== null) {
+			return $this->user;
+		}
+		$userId = $_SESSION['user'] ?? null;
+		if (!$userId) {
+			return null;
+		}
+		$user = $this->entityManager->getRepository(User::class)->find($userId);
+
+		if (!$user) {
+			return null;
+		}
+		$this->user = $user;
 		return $this->user;
 	}
-	$userId = $_SESSION['user'] ?? null;
-	if(! $userId) {
-		return null;
+	public function attemptLogin(array $credentials): bool
+	{
+		$user = $this->entityManager->getRepository(User::class)->findOneBy(
+			['email' => $credentials['email']]
+		);
+		if (!$user || !$this->checkCredentials($user, $credentials)) {
+			return false;
+		}
+		session_regenerate_id();
+		$_SESSION['user'] = $user->getId();
+		$this->user = $user;
+		return true;
 	}
-	$user = $this->entityManager->getRepository(User::class)->find($userId);
 
-	if(! $user) {
-		return null;
+	public function checkCredentials(UserInterface $user, array $credentials): bool
+	{
+		return password_verify($credentials['password'], $user->getPassword());
+
 	}
-	$this->user = $user;
-	return $this->user;
- }
+	public function logOut(): void
+	{
+		unset($_SESSION['user']);
+		$this->user = null;
+	}
+
+
+
+
 }
