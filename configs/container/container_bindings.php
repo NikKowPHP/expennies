6ab020e48a0a0b16758005c1ce366ac76a6093ee
NikKowPHP <nik.kow@outlook.com>
2024-01-26 13:07:12 +0100

    chore: register user provider service interface in container

--- configs/container/container_bindings.php
+++ configs/container/container_bindings.php
@@ -14,12 +14,14 @@ use Doctrine\ORM\EntityManager;
 use App\Contracts\AuthInterface;
 use Twig\Extra\Intl\IntlExtension;
 use Symfony\Component\Asset\Package;
+use App\Services\UserProviderService;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Asset\Packages;
 use Psr\Http\Message\ResponseFactoryInterface;
+use App\Contracts\UserProviderServiceInterface;
+
 use Symfony\Bridge\Twig\Extension\AssetExtension;
 use Symfony\WebpackEncoreBundle\Asset\TagRenderer;
-
 use Symfony\WebpackEncoreBundle\Asset\EntrypointLookup;
 use Symfony\WebpackEncoreBundle\Twig\EntryFilesTwigExtension;
 use Symfony\Component\Asset\VersionStrategy\JsonManifestVersionStrategy;
@@ -69,5 +71,8 @@ return [
         $container->get('webpack_encore.packages')
     ),
     ResponseFactoryInterface::class => fn(App $app) => $app->getResponseFactory(),
-    AuthInterface::class => fn(ContainerInterface $container) => $container->get(Auth::class)
+    AuthInterface::class => fn(ContainerInterface $container) => $container->get(Auth::class),
+
+    UserProviderServiceInterface::class => fn(ContainerInterface $container) => $container->get(UserProviderService::class),
+
 ];
