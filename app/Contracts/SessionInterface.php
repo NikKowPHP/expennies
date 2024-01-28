9e413ddd569768258dd0257ce1edbca6e76c5902
NikKowPHP <nik.kow@outlook.com>
2024-01-28 08:36:16 +0100

    fix(SessionINterface): Change return type of has function

--- app/Contracts/SessionInterface.php
+++ app/Contracts/SessionInterface.php
@@ -10,7 +10,7 @@ interface SessionInterface
 	public function get(string $key, mixed $default = null): mixed;
 	public function regenerate(): bool;
 	public function put(string $key, mixed $value): void;
-	public function has(string $key): void;
+	public function has(string $key): bool;
 	public function flash(string $key, array $messages): void;
 	public function getFlash(string $key): array;
 
