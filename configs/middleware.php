b067663fcd0084ec6d956234e14319c6941d5f86
NikKowPHP <nik.kow@outlook.com>
2024-01-29 15:31:47 +0100

    feat: Introduce Csrf fields middleware for csrf protection make functioning

--- configs/middleware.php
+++ configs/middleware.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 use App\Config;
 use App\Middleware\AuthenticateMiddleware;
+use App\Middleware\CsrfFieldsMiddleware;
 use App\Middleware\OldFormMiddleware;
 use App\Middleware\StartSessionsMiddleware;
 use App\Middleware\ValidationErrosMiddleware;
@@ -16,6 +17,7 @@ return function (App $app) {
     $container = $app->getContainer();
     $config    = $container->get(Config::class);
 
+    $app->add(CsrfFieldsMiddleware::class);
     $app->add('csrf');
     $app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
     $app->add(ValidationExceptionMiddleware::class);
