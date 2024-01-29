cfa633467b4cbe311c5b7ffab2a8e9485b1f8280
NikKowPHP <nik.kow@outlook.com>
2024-01-29 15:40:38 +0100

    feat:Display username and logout btn in nav

--- app/Middleware/AuthMiddleware.php
+++ app/Middleware/AuthMiddleware.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 namespace App\Middleware;
 
+use Slim\Views\Twig;
 use App\Contracts\AuthInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -12,12 +13,14 @@ use Psr\Http\Message\ResponseFactoryInterface;
 
 class AuthMiddleware implements MiddlewareInterface
 {
-	public function __construct(private readonly ResponseFactoryInterface $responseFactory, private readonly AuthInterface $auth)
+	public function __construct(private readonly ResponseFactoryInterface $responseFactory, private readonly AuthInterface $auth,
+	private readonly Twig $twig)
 	{
 	}
 	public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
 	{
 		if ($user = $this->auth->user()) {
+			$this->twig->getEnvironment()->addGlobal('auth', ['id' => $user->getId(), 'name' => $user->getName()]);
 			return $handler->handle($request->withAttribute('user', $user));
 
 		}
