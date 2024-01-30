89e173e183b8bfa449abdae3a5963c043da342ee
NikKowPHP <nik.kow@outlook.com>
2024-01-30 11:53:40 +0100

    define endpoints for categories crud

--- app/Controllers/CategoriesController.php
+++ app/Controllers/CategoriesController.php
@@ -1,38 +1,46 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace App\Controllers;
 
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
 use Slim\Views\Twig;
+use App\Services\CategoryService;
+use App\Contracts\RequestValidatorFactoryInterface;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use App\RequestValidators\CreateCategoryRequestValidator;
 
 class CategoriesController
 {
-    public function __construct(private readonly Twig $twig)
-    {
-    }
+	public function __construct(private readonly Twig $twig, private readonly RequestValidatorFactoryInterface $requestValidatorFactory, private readonly CategoryService $categoryService)
+	{
+	}
 
-    public function index(Request $request, Response $response): Response
-    {
-        return $this->twig->render(
-            $response,
-            'categories/index.twig',
-        );
-    }
+	public function index(Request $request, Response $response): Response
+	{
+		return $this->twig->render(
+			$response,
+			'categories/index.twig',
+			[
+				'categories' => $this->categoryService->getAll(),
+			]
+		);
+	}
 
-    public function store(Request $request, Response $response): Response
-    {
-        // TODO
+	public function store(Request $request, Response $response): Response
+	{
+		$data = $this->requestValidatorFactory->make(CreateCategoryRequestValidator::class)->validate($request->getParsedBody());
 
-        return $response->withHeader('Location', '/categories')->withStatus(302);
-    }
+		$this->categoryService->create($data['name'], $request->getAttribute('user'));
 
-    public function delete(Request $request, Response $response): Response
-    {
-        // TODO
+		return $response->withHeader('Location', '/categories')->withStatus(302);
+	}
 
-        return $response->withHeader('Location', '/categories')->withStatus(302);
-    }
+	public function delete(Request $request, Response $response): Response
+	{
+		// TODO
+
+		return $response->withHeader('Location', '/categories')->withStatus(302);
+	}
 }
