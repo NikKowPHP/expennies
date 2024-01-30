1b5f7f536a5f6890c25afc9cd85d709322bbb3dd
NikKowPHP <nik.kow@outlook.com>
2024-01-30 13:08:50 +0100

    add method override middleware to middleware register class

--- configs/middleware.php
+++ configs/middleware.php
@@ -3,13 +3,13 @@
 declare(strict_types = 1);
 
 use App\Config;
-use App\Middleware\AuthenticateMiddleware;
 use App\Middleware\CsrfFieldsMiddleware;
 use App\Middleware\OldFormMiddleware;
 use App\Middleware\StartSessionsMiddleware;
 use App\Middleware\ValidationErrosMiddleware;
 use App\Middleware\ValidationExceptionMiddleware;
 use Slim\App;
+use Slim\Middleware\MethodOverrideMiddleware;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 
@@ -17,6 +17,7 @@ return function (App $app) {
     $container = $app->getContainer();
     $config    = $container->get(Config::class);
 
+    $app->add(MethodOverrideMiddleware::class);
     $app->add(CsrfFieldsMiddleware::class);
     $app->add('csrf');
     $app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
