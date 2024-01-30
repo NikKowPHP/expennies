5c580fcdfce3ed367cfc2c112c5e7d3be09c18a8
NikKowPHP <nik.kow@outlook.com>
2024-01-30 14:36:30 +0100

    make open an edit category modal

--- resources/js/categories.js
+++ resources/js/categories.js
@@ -1,30 +1,39 @@
-import { Modal } from "bootstrap"
+import { Modal } from "bootstrap";
 
-window.addEventListener('DOMContentLoaded', function () {
-    const editCategoryModal = new Modal(document.getElementById('editCategoryModal'))
+window.addEventListener("DOMContentLoaded", function () {
+  const editCategoryModal = new Modal(
+    document.getElementById("editCategoryModal")
+  );
 
-    document.querySelectorAll('.edit-category-btn').forEach(button => {
-        button.addEventListener('click', function (event) {
-            const categoryId = event.currentTarget.getAttribute('data-id')
+  document.querySelectorAll(".edit-category-btn").forEach((button) => {
+    button.addEventListener("click", function (event) {
+      const categoryId = event.currentTarget.getAttribute("data-id");
 
-            openEditCategoryModal(editCategoryModal, {id: categoryId, name: ''})
-        })
-    })
+			console.log(editCategoryModal)
+      fetch(`/categories/${categoryId}`)
+        .then((response) => response.json())
+        .then((response) =>
+          openEditCategoryModal(editCategoryModal, response)
+        );
+    });
+  });
 
-    document.querySelector('.save-category-btn').addEventListener('click', function (event) {
-        const categoryId = event.currentTarget.getAttribute('data-id')
+  document
+    .querySelector(".save-category-btn")
+    .addEventListener("click", function (event) {
+      const categoryId = event.currentTarget.getAttribute("data-id");
 
-        // TODO: Post update to the category
-        console.log(categoryId)
-    })
-})
+    });
+});
 
-function openEditCategoryModal(modal, {id, name}) {
-    const nameInput = modal._element.querySelector('input[name="name"]')
+function openEditCategoryModal(modal, { id, name }) {
+  const nameInput = modal._element.querySelector('input[name="name"]');
 
-    nameInput.value = name
+  nameInput.value = name;
 
-    modal._element.querySelector('.save-category-btn').setAttribute('data-id', id)
+  modal._element
+    .querySelector(".save-category-btn")
+    .setAttribute("data-id", id);
 
-    modal.show()
+  modal.show();
 }
