6440440c1f52f804e0832bdf8d8c288a1457a4fd
NikKowPHP <nik.kow@outlook.com>
2024-01-31 08:49:14 +0100

    add flags prop to jsonencode

--- app/ResponseFormatter.php
+++ app/ResponseFormatter.php
@@ -10,7 +10,7 @@ class ResponseFormatter
 	public function asJson(ResponseInterface $response, mixed $data, int $flags = JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_QUOT | JSON_HEX_APOS | JSON_THROW_ON_ERROR): ResponseInterface
 	{
 		$response = $response->withHeader('Content-Type', 'application/json');
-		$response->getBody()->write(json_encode($data));
+		$response->getBody()->write(json_encode($data, $flags));
 		return $response;
 	}
 }
