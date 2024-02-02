848a6c2e7a41c2eb22c24b8ae0efe9ed99cac0d6
NikKowPHP <nik.kow@outlook.com>
2024-02-02 16:24:32 +0100

    create a fetch request to category updation endpoint and retrieve the data. create getCsrfFileds funciton to dynamically take the values of csrf tags

--- resources/js/categories.js
+++ resources/js/categories.js
@@ -12,7 +12,7 @@ window.addEventListener("DOMContentLoaded", function () {
       fetch(`/categories/${categoryId}`)
         .then((response) => response.json())
         .then((jsonData) => {
-          openEditCategoryModal(editCategoryModal, jsonData)
+          openEditCategoryModal(editCategoryModal, jsonData);
         });
     });
   });
@@ -21,8 +21,36 @@ window.addEventListener("DOMContentLoaded", function () {
     .querySelector(".save-category-btn")
     .addEventListener("click", function (event) {
       const categoryId = event.currentTarget.getAttribute("data-id");
+
+      fetch(`/categories/${categoryId}`, {
+        method: "POST",
+        body: JSON.stringify({
+          name: editCategoryModal._element.querySelector('input[name="name"]')
+            .value,
+          ...getCsrfFields()
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => console.log(response))
+        .then((responseText) => console.log(responseText));
     });
 });
+
+function getCsrfFields() {
+  const csrfNameField = document.querySelector("#csrfName");
+  const csrfValueField = document.querySelector("#csrfValue");
+  const csrfNameKey = csrfNameField.getAttribute("name");
+  const csrfValueKey = csrfValueField.getAttribute("name");
+  const csrfName = csrfNameField.content;
+  const csrfValue = csrfValueField.content;
+
+  return {
+    [csrfNameKey]: csrfName,
+    [csrfValueKey]: csrfValue
+  }
+}
 
 function openEditCategoryModal(modal, { id, name }) {
   const nameInput = modal._element.querySelector('input[name="name"]');
