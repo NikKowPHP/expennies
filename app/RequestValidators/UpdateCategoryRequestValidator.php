af8b102ca41e93b002eae07d99d7b6909c1a94bc
NikKowPHP <nik.kow@outlook.com>
2024-02-02 16:32:51 +0100

    create updateCategory request validator to validate the updation of the category

---
+++ app/RequestValidators/UpdateCategoryRequestValidator.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+namespace App\RequestValidators;
+
+use App\Contracts\RequestValidatorInterface;
+use Valitron\Validator;
+use Doctrine\ORM\EntityManager;
+use App\Exception\ValidationException;
+
+class   UpdateCategoryRequestValidator implements RequestValidatorInterface
+{
+
+	public function __construct(private readonly EntityManager $entityManager)
+	{
+
+	}
+
+	public function validate(array $data): array
+	{
+		$v = new Validator($data);
+		$v->rule('required', 'name');
+		$v->rule('lengthMax', 'name', 50);
+
+		if (!$v->validate()) {
+			throw new ValidationException($v->errors());
+		}
+		return $data;
+	}
+
+}
