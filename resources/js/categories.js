35a64e459216e312fe97825f3437230d801b6315
NikKowPHP <nik.kow@outlook.com>
2024-02-03 10:45:34 +0100

    update get and post requests, make post request of categories parse the dom element along with the request

--- resources/js/categories.js
+++ resources/js/categories.js
@@ -10,9 +10,11 @@ window.addEventListener("DOMContentLoaded", function () {
     button.addEventListener("click", function (event) {
       const categoryId = event.currentTarget.getAttribute("data-id");
 
-      get(`/categories/${categoryId}`).then((jsonData) => {
-        openEditCategoryModal(editCategoryModal, jsonData);
-      });
+      get(`/categories/${categoryId}`)
+        .then((response) => response.json())
+        .then((response) => {
+          openEditCategoryModal(editCategoryModal, response);
+        });
     });
   });
 
@@ -21,26 +23,20 @@ window.addEventListener("DOMContentLoaded", function () {
     .addEventListener("click", function (event) {
       const categoryId = event.currentTarget.getAttribute("data-id");
 
-      post(`/categories/${categoryId}`, {
-        name: editCategoryModal._element.querySelector('input[name="name"]')
-          .value,
-      }).then((response) => console.log(response));
+      post(
+        `/categories/${categoryId}`,
+        {
+          name: editCategoryModal._element.querySelector('input[name="name"]')
+            .value,
+        },
+        editCategoryModal._element
+      ).then((response) => {
+        if (response.ok) {
+          editCategoryModal.hide();
+        }
+      });
     });
 });
-
-function getCsrfFields() {
-  const csrfNameField = document.querySelector("#csrfName");
-  const csrfValueField = document.querySelector("#csrfValue");
-  const csrfNameKey = csrfNameField.getAttribute("name");
-  const csrfValueKey = csrfValueField.getAttribute("name");
-  const csrfName = csrfNameField.content;
-  const csrfValue = csrfValueField.content;
-
-  return {
-    [csrfNameKey]: csrfName,
-    [csrfValueKey]: csrfValue,
-  };
-}
 
 function openEditCategoryModal(modal, { id, name }) {
   const nameInput = modal._element.querySelector('input[name="name"]');
