47a90e97b59f7ab3a354231e1d8dbe986de01c45
NikKowPHP <nik.kow@outlook.com>
2024-02-04 08:30:51 +0100

    disable notifications in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ Encore
      * https://symfony.com/doc/current/frontend.html#adding-more-features
      */
     .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
+    .enableBuildNotifications(false)
     .enableSourceMaps(! Encore.isProduction())
 
     // enables hashed filenames (e.g. app.abc123.css)
