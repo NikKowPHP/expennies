018ab4c9fd926b06e1f6a4873d24fb8380098ecb
NikKowPHP <nik.kow@outlook.com>
2024-02-05 18:26:19 +0100

    introduce getDataTableQueryParameters method to process the request and return dto

--- app/Services/RequestService.php
+++ app/Services/RequestService.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 namespace App\Services;
 
 use App\Contracts\SessionInterface;
+use App\DataObjects\DataTableQueryParams;
 use Psr\Http\Message\ServerRequestInterface;
 
 
@@ -24,9 +25,28 @@ class RequestService
 		}
 		return $referer;
 	}
-	public function isXhr(ServerRequestInterface $request):bool
+	public function isXhr(ServerRequestInterface $request): bool
 	{
 		return $request->getHeaderLine('X-Requested-With') === 'XMLHttpRequest';
+	}
+
+	public function getDataTableQueryParameters(ServerRequestInterface $request): DataTableQueryParams
+	{
+
+		$params = $request->getQueryParams();
+
+		$orderBy = $params['columns'][$params['order'][0]['column']]['data'];
+		$orderDir = $params['order'][0]['dir'];
+
+		return new DataTableQueryParams(
+			(int) $params['start'],
+			(int) $params['length'],
+			$orderBy,
+			$orderDir,
+			$params['search']['value'],
+			(int) $params['draw'],
+		);
+
 	}
 
 }
