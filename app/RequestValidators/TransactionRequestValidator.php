841dd19b651bb97dc31718120f0b163eac8a4571
NikKowPHP <nik.kow@outlook.com>
2024-02-10 10:10:50 +0100

    refactor(TransactionRequestValidator): Update the validator based on transaction data

--- app/RequestValidators/TransactionRequestValidator.php
+++ app/RequestValidators/TransactionRequestValidator.php
@@ -1,49 +1,54 @@
 <?php
-declare(strict_types=1);
+
+declare(strict_types = 1);
+
 namespace App\RequestValidators;
 
 use App\Contracts\RequestValidatorInterface;
+use App\Exception\ValidationException;
 use App\Services\CategoryService;
 use Valitron\Validator;
-use App\Exception\ValidationException;
 
-class  TransactionRequestValidator  implements RequestValidatorInterface
+class TransactionRequestValidator implements RequestValidatorInterface
 {
+    public function __construct(protected readonly CategoryService $categoryService)
+    {
+    }
 
-	public function __construct( private readonly CategoryService $categoryService)
-	{
+    public function validate(array $data): array
+    {
+        $v = new Validator($data);
 
-	}
+        $v->rule('required', ['description', 'amount', 'date', 'category']);
+        $v->rule('lengthMax', 'description', 255);
+        $v->rule('dateFormat', 'dateFormat', 'm/d/Y g:i A');
+        $v->rule('numeric', 'amount');
+        $v->rule('integer', 'category');
+        $v->rule(
+            function($field, $value, $params, $fields) use (&$data) {
+                $id = (int) $value;
 
-	public function validate(array $data): array
-	{
-		$v = new Validator($data);
-		$v->rule('required', ['description', 'amount', 'date', 'category']);
-		$v->rule('lengthMax', 'description', 255);
-		$v->rule('dateFormat', 'dateFormat', 'm/d/Y g:i A');
-		$v->rule('numeric', 'amount');
-		$v->rule('integer', 'category');
-		$v->rule
-		(
-			function($field, $value, $params, $fields) use(&$data) {
-				$id = (int) $value;
-				if(! $id) {
-					return false;
-				}
-				$category = $this->categoryService->getById($id);
-				if($category) {
-					$data['category'] = $category;
-					return true;
-				}
-				return false;
-			},
-			'category'
-		)->message('Category not found');
+                if (! $id) {
+                    return false;
+                }
 
-		if (!$v->validate()) {
-			throw new ValidationException($v->errors());
-		}
-		return $data;
-	}
+                $category = $this->categoryService->getById($id);
 
+                if ($category) {
+                    $data['category'] = $category;
+
+                    return true;
+                }
+
+                return false;
+            },
+            'category'
+        )->message('Category not found');
+
+        if (! $v->validate()) {
+            throw new ValidationException($v->errors());
+        }
+
+        return $data;
+    }
 }
