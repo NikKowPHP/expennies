8a06c06444f8637e85de3508b0ac600649c3ab66
NikKowPHP <nik.kow@outlook.com>
2024-02-18 11:16:15 +0100

    define routeEntityBinding class

--- app/RouteEntityBindingStrategy.php
+++ app/RouteEntityBindingStrategy.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace App;
 
@@ -15,22 +15,66 @@ use Slim\Interfaces\InvocationStrategyInterface;
 
 class RouteEntityBindingStrategy implements InvocationStrategyInterface
 {
-    public function __construct(
-        private readonly EntityManagerServiceInterface $entityManagerService,
-        private readonly ResponseFactoryInterface $responseFactory
-    ) {
-    }
+	public function __construct(
+		private readonly EntityManagerServiceInterface $entityManagerService,
+		private readonly ResponseFactoryInterface $responseFactory,
+	) {
+	}
 
-    public function __invoke(
-        callable $callable,
-        ServerRequestInterface $request,
-        ResponseInterface $response,
-        array $routeArguments
-    ): ResponseInterface
-		{
+	public function __invoke(
+		callable $callable,
+		ServerRequestInterface $request,
+		ResponseInterface $response,
+		array $routeArguments
+	): ResponseInterface {
 
+		$callableReflection = $this->createReflectionForCallable($callable);
+		$resolvedArguments = [];
 
-			return $callable($request, $response, $routeArguments);
+		foreach ($callableReflection->getParameters() as $parameter) {
+			$type = $parameter->getType();
+			if (!$type) {
+				continue;
+			}
+
+			$paramName = $parameter->getName();
+			$typeName = $type->getName();
+
+			if ($type->isBuiltin()) {
+				if ($typeName === 'array' && $paramName === 'args') {
+					$resolvedArguments[] = $routeArguments;
+				}
+
+			} else {
+				if ($typeName === ServerRequestInterface::class) {
+					$resolvedArguments[] = $request;
+				} else if ($typeName === ResponseInterface::class) {
+					$resolvedArguments[] = $response;
+				} else {
+					$entityId = $routeArguments[$paramName] ?? null;
+					if( ! $entityId || $parameter->allowsNull()) {
+						throw new \InvalidArgumentException('Unable to resolve argument "' . $paramName . '" in the callable');
+					}
+					$entity = $this->entityManagerService->find($typeName, $entityId);
+					if(! $entity) {
+						return $this->responseFactory->createResponse(404);
+					}
+					$resolvedArguments[] = $entity;
+
+				}
+			}
 
 		}
+
+
+		return $callable(...$resolvedArguments);
+
 	}
+
+	public function createReflectionForCallable(callable $callable)
+	{
+		return is_array($callable)
+			? new ReflectionMethod($callable[0], $callable[1])
+			: new ReflectionFunction($callable);
+	}
+}
