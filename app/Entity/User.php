1961cf1c7c9d55e7fe86309e1259aacdb2a6b5fd
NikKowPHP <nik.kow@outlook.com>
2024-02-22 15:50:37 +0100

    add verifyAt prop

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -1,5 +1,4 @@
-<?php
-declare(strict_types=1);
+<?php declare(strict_types=1);
 namespace App\Entity;
 
 use App\Contracts\UserInterface;
@@ -17,123 +16,126 @@ use Doctrine\ORM\Mapping\Table;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Id;
 
-
 #[Entity, Table('users')]
 #[HasLifecycleCallbacks]
 class User implements UserInterface
 {
-	use HasTimestamps;
+    use HasTimestamps;
 
-	#[Id, Column(options: ['unsigned' => true]), GeneratedValue]
-	private int $id;
+    #[Id, Column(options: ['unsigned' => true]), GeneratedValue]
+    private int $id;
 
-	#[Column]
-	private string $name;
+    #[Column]
+    private string $name;
 
-	#[Column]
-	private string $email;
+    #[Column]
+    private string $email;
 
-	#[Column]
-	private string $password;
+    #[Column]
+    private string $password;
 
-	#[OneToMany(mappedBy: 'user', targetEntity: Transaction::class)]
-	private Collection $transactions;
+    #[Column(name: 'verified_at', nullable: true)]
+    private ?\DateTime $verifiedAt;
 
-	#[OneToMany(mappedBy: 'user', targetEntity: Category::class)]
-	private Collection $categories;
+    #[OneToMany(mappedBy: 'user', targetEntity: Transaction::class)]
+    private Collection $transactions;
 
-	public function __construct()
-	{
-		$this->transactions = new ArrayCollection();
-		$this->categories = new ArrayCollection();
+    #[OneToMany(mappedBy: 'user', targetEntity: Category::class)]
+    private Collection $categories;
 
-	}
+    public function __construct()
+    {
+        $this->transactions = new ArrayCollection();
+        $this->categories = new ArrayCollection();
+    }
 
-	public function getId(): int
-	{
-		return $this->id;
-	}
+    public function getId(): int
+    {
+        return $this->id;
+    }
 
-	#[PrePersist, PreUpdate]
-	public function updateTimestamps(LifecycleEventArgs $args): void
-	{
-		if (!isset($this->createdAt)) {
-			$this->createdAt = new \DateTime();
-		}
-		$this->updatedAt = new \DateTime();
+    #[PrePersist, PreUpdate]
+    public function updateTimestamps(LifecycleEventArgs $args): void
+    {
+        if (!isset($this->createdAt)) {
+            $this->createdAt = new \DateTime();
+        }
+        $this->updatedAt = new \DateTime();
+    }
 
-	}
+    public function getName(): string
+    {
+        return $this->name;
+    }
 
-	public function getName(): string
-	{
-		return $this->name;
-	}
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
 
-	public function setName(string $name): void
-	{
-		$this->name = $name;
-	}
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
 
-	public function getEmail(): string
-	{
-		return $this->email;
-	}
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
 
-	public function setEmail(string $email): void
-	{
-		$this->email = $email;
-	}
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
 
-	public function getPassword(): string
-	{
-		return $this->password;
-	}
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
 
-	public function setPassword(string $password): void
-	{
-		$this->password = $password;
-	}
+    public function getTransactions(): Collection
+    {
+        return $this->transactions;
+    }
 
-	public function getTransactions(): Collection
-	{
-		return $this->transactions;
-	}
+    public function addTransaction(Transaction $transaction): User
+    {
+        if (!$this->transactions->contains($transaction)) {
+            $this->transactions->add($transaction);
+            return $this;
+        }
+        return $this;
+    }
 
-	public function addTransaction(Transaction $transaction): User
-	{
-		if (!$this->transactions->contains($transaction)) {
-			$this->transactions->add($transaction);
-			return $this;
-		}
-		return $this;
-	}
+    public function removeTransaction(Transaction $transaction): void
+    {
+        if ($this->transactions->removeElement($transaction)) {
+            $transaction->setUser(null);
+        }
+    }
 
-	public function removeTransaction(Transaction $transaction): void
-	{
-		if ($this->transactions->removeElement($transaction)) {
-			$transaction->setUser(null);
-		}
-	}
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
 
-	public function getCategories(): Collection
-	{
-		return $this->categories;
-	}
+    public function addCategory(Category $category): User
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories->add($category);
+            return $this;
+        }
+        return $this;
+    }
 
-	public function addCategory(Category $category): User
-	{
-		if (!$this->categories->contains($category)) {
-			$this->categories->add($category);
-			return $this;
-		}
-		return $this;
-	}
-
-	public function removeCategory(Category $category): User
-	{
-		if ($this->categories->removeElement($category)) {
-			$category->setUser(null);
-		}
-	}
-
+    public function removeCategory(Category $category): User
+    {
+        if ($this->categories->removeElement($category)) {
+            $category->setUser(null);
+        }
+    }
+    public function getVerifiedAt(): ?\DateTimeInterface
+    {
+        return $this->verifiedAt;
+    }
 }
