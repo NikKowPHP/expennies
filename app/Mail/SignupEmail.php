26920550988c48555e8613adb673d798666e5862
NikKowPHP <nik.kow@outlook.com>
2024-02-22 14:53:05 +0100

    feat: Introduce signup email class

---
+++ app/Mail/SignupEmail.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types= 1);
+namespace App\Mail;
+
+use App\Config;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+
+class SignupEmail
+{
+    public function __construct(
+        private readonly Config $config,
+    ) {}
+    public function send(string $to): void
+    {
+        $message = (new TemplatedEmail())
+        ->from($this->config->get('mailer.from'))
+        ->to($to)
+        ->subject('Welcome to expennies')
+        ->htmlTemplate('emails/signup.html.twig')
+        ->context([]);
+    }
+}
