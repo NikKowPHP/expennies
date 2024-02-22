d0de65d0bb36a99dd681e7fed6a37276560ae44d
NikKowPHP <nik.kow@outlook.com>
2024-02-22 15:08:10 +0100

    feat: Define the send method in signup email class with provided context

--- app/Mail/SignupEmail.php
+++ app/Mail/SignupEmail.php
@@ -3,11 +3,15 @@ namespace App\Mail;
 
 use App\Config;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\BodyRendererInterface;
 
 class SignupEmail
 {
     public function __construct(
         private readonly Config $config,
+        private readonly MailerInterface $mailer,
+        private readonly BodyRendererInterface $bodyRenderer,
     ) {}
     public function send(string $to): void
     {
@@ -16,6 +20,11 @@ class SignupEmail
         ->to($to)
         ->subject('Welcome to expennies')
         ->htmlTemplate('emails/signup.html.twig')
-        ->context([]);
+        ->context([
+            'activationLink' => "#",
+            'expirationDate' => new \DateTime('+30 minutes')
+        ]);
+        $this->bodyRenderer->render($message);
+        $this->mailer->send($message);
     }
 }
