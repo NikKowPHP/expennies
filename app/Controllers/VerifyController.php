09767f528afbc0fa04c7bfc5b40b80ca8169f584
NikKowPHP <nik.kow@outlook.com>
2024-02-23 15:58:46 +0100

    update verify method in verify controller , add conditions and redirect user to the index

--- app/Controllers/VerifyController.php
+++ app/Controllers/VerifyController.php
@@ -2,24 +2,40 @@
 
 namespace App\Controllers;
 
+use App\Contracts\UserProviderServiceInterface;
+use App\Entity\User;
 use Slim\Views\Twig;
-use App\Contracts\RequestValidatorFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use App\Contracts\RequestValidatorFactoryInterface;
 
 class VerifyController
 {
     public function __construct(
         private readonly Twig $twig,
         private readonly RequestValidatorFactoryInterface $requestValidatorFactory,
+        private readonly UserProviderServiceInterface $userProviderService,
     ) {}
     public function index(ResponseInterface $response): ResponseInterface
     {
         return $this->twig->render($response, "auth/verify.twig");
     }
-    public function verify(ServerRequestInterface $request,ResponseInterface $response): ResponseInterface
+    public function verify(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
+        /** @var User $user */
+        $user = $request->getAttribute("user");
 
-        return $this->twig->render($response, "auth/verify.twig");
+        if (! hash_equals((string) $user->getId(), $args["id"]) || !hash_equals(sha1($user->getEmail()), $args['hash'])) {
+            throw new \RuntimeException('Verification failed');
+        }
+
+        if (! $user->getVerifiedAt()) {
+            $this->userProviderService->verifyUser($user);
+        }
+
+
+
+
+        return $response->withHeader('Location', '/')->withStatus(302);
     }
 }
